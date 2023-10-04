f832f9239764e9cec611bbaa75fb960a0072e513
Felipe <felipechaves.pro@gmail.com>
2023-10-04 15:51:49 -0300

    return to past auth secret

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     jwt: {
-        secret: process.env.AUTH_SECRET || "default4333",
+        secret: process.env.AUTH_SECRET || "default3333",
         expiresIn: "1d"
     }
 }
